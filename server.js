a1e460a4a36775e57a0008fbcc9b17908aee377d
farooqsm <shaikfaroq@gmail.com>
2016-10-28 22:38:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     
     var pool = new Pool(config);
     app.get('/test-db',function(req,res){
-        pllo.query('SELECT * FROM test',function(err,result){
+        pool.query('SELECT * FROM test',function(err,result){
             if(err)
             {
                 res.status(500).send(err,toString());
