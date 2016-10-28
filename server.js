3d25e0f72ea6691a01510578719f683133644a15
farooqsm <shaikfaroq@gmail.com>
2016-10-28 22:30:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,6 +67,20 @@
     app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname, 'ui', 'index.html'));
     });
+    
+    var pool = new Pool(config);
+    app.get('/test-db',function(req,res){
+        pllo.query('SELECT * FROM test',function(err,result){
+            if(err)
+            {
+                res.status(500).send(err,toString());
+            }else
+            {
+                res.send(JSON.stringify(result));
+            }
+        });
+    });
+    
     app.get('/article-one2', function (req, res) {
       res.sendFile(path.join(__dirname, 'ui', 'article-one2.html'));
     }); 
